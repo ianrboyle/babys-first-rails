827bb08b031153746edb6b92e276293385c2f728
Ian Boyle <irboyle21@gmail.com>
2021-10-13 10:11:25 -0400

    initial commit. adds welcome path / welcome method and hello path / hello method

---
+++ app/controllers/welcomes_controller.rb
@@ -0,0 +1,9 @@
+class WelcomesController < ApplicationController
+  def hello_method
+    render json: {message: "Hello from welcomes controller!"}
+  end
+
+  def about_method
+    render json: {message: "Ruby is my favorite computing language"}
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "/welcome_path", controller: "welcomes", action: "hello_method"
+  get "about_path" => "welcomes#about_method"
+end
